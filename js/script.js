f3383e7bdc2ec6c5b1bee8e7f5d9b63b3c4d5cf0
Martin Vedriš <martinvedris.email@gmail.com>
2022-05-06 13:48:41 +0200

    function playRound with game logic - implemented

--- js/script.js
+++ js/script.js
@@ -9,6 +9,10 @@ function computerPlay() {
 function playerPlay() {
     /* Get playerInput from a prompt */
     let playerInput = prompt("Play a game of rock, paper, scissors", "rock, paper, scissors");
+    /* Check if playerInput was canceled */
+    if (playerInput === null) {
+        return;
+    }
     /* Turn playerInput to playerSelection so it's case-insensitive */
     let playerSelection = playerInput.toLowerCase();
 
@@ -21,9 +25,28 @@ function playerPlay() {
         case "scissors":
             break;
         default:
-            console.log("INVALID INPUT!");
             return;
     }
 
     return playerSelection;
+}
+
+/* Function plays single round of rock, paper, scissors */
+function playRound(playerSelection, computerSelection) {
+    /* Check if playerSelection is undefined */
+    while (playerSelection === undefined) {
+        playerSelection = playerPlay();
+    }
+
+    /* Rock, paper, scissors - logic */
+    if (playerSelection === computerSelection) {
+        // TIE 
+       return(`Game tied - both player selected ${playerSelection}`);
+    } else if ((playerSelection === "rock" && computerSelection === "scissors") || (playerSelection === "paper" && computerSelection === "rock") || (playerSelection === "scissors" && computerSelection === "paper")) {
+        // PLAYER WIN 
+       return(`You win - ${playerSelection} beats ${computerSelection}`);
+    } else {
+        // COMPUTER WIN
+       return(`You lose - ${computerSelection} beats ${playerSelection}`);
+    }
 }
